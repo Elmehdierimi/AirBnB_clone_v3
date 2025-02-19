b0cbc71ebbe015f330a9fe3dd440342e149acc34
elmehdi <you@example.com>
2025-02-19 04:39:05 +0000

    jfhjbfrf

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""View to handle API actions related to State objects
+"""View for states to handles the api
 """
 
 from api.v1.views import app_views
@@ -11,24 +11,24 @@ from models import storage
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
 def states_get(state_id=None):
-    """Manipulate State object by state_id, or all objects if
-    state_id is None
+    """if state id is none manipulate
+    state id
     """
     from models.state import State
     states = storage.all(State)
 
-    # GET REQUESTS
+    # GETTING REQUESTS
     if request.method == 'GET':
-        if not state_id:  # if no, state id specified, return all
+        if not state_id:  # No return all
             return jsonify([obj.to_dict() for obj in states.values()])
 
         key = 'State.' + state_id
-        try:  # if obj exists in dictionary, convert from obj -> dict -> json
+        try:  # convert from obj to dict to json
             return jsonify(states[key].to_dict())
         except KeyError:
-            abort(404)  # if State of state_id does not exist
+            abort(404)  # if stateid doesnt exist
 
-    # DELETE REQUESTS
+    # DELETTING REQUESTS
     elif request.method == 'DELETE':
         try:
             key = 'State.' + state_id
@@ -38,30 +38,30 @@ def states_get(state_id=None):
         except:
             abort(404)
 
-    # POST REQUESTS
+    # POSTTING REQUESTS
     elif request.method == 'POST':
-        # convert JSON request to dict
+        # JSON to dict
         if request.is_json:
             body_request = request.get_json()
         else:
             abort(400, 'Not a JSON')
 
-        # instantiate, store, and return new State object
+        # return new State object
         if 'name' in body_request:
             new_state = State(**body_request)
             storage.new(new_state)
             storage.save()
             return jsonify(new_state.to_dict()), 201
-        else:  # if request does not contain required attribute
+        else:  # if it doesnt contain attribute
             abort(400, 'Missing name')
 
-    # PUT REQUESTS
+    # PUTTING REQUESTS
     elif request.method == 'PUT':
         key = 'State.' + state_id
         try:
             state = states[key]
 
-            # convert JSON request to dict
+            # JSON to dict
             if request.is_json:
                 body_request = request.get_json()
             else:
@@ -77,6 +77,6 @@ def states_get(state_id=None):
         except KeyError:
             abort(404)
 
-    # UNSUPPORTED REQUESTS
+    # UNSUPPORTTING
     else:
         abort(501)
